1eac69959527aa88b52aae4956665507f966624e
Fan2 <alejandrofan2@gmail.com>
2023-01-12 10:50:29 +0100

    add checks for input file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { readFile, writeFile } = require('node:fs').promises;
 const { join } = require('path');
 require('dotenv').config();
 
-const JSON_LIST_FILE_NAME = process.env.JSON_LIST_FILE_NAME || 'data.json';
+const JSON_LIST_FILE_NAME = process.env.JSON_LIST_FILE_NAME || null;
 const DB_CONFIG = {
     "host": process.env.DB_HOST,
     "user": process.env.DB_USER,
@@ -13,6 +13,10 @@ const DB_CONFIG = {
 };
 
 async function main() {
+    if (JSON_LIST_FILE_NAME === null) {
+        console.error('JSON_LIST_FILE_NAME not set in .env file.');
+        process.exit(5);
+    }
 
     const connection = await sql.createConnection(DB_CONFIG).catch((error) => {
         console.error(error);
@@ -21,12 +25,21 @@ async function main() {
 
     const filePath = join(__dirname, JSON_LIST_FILE_NAME);
 
-    const file = await readFile(filePath, 'utf-8');
-    const parsedFileList = JSON.parse(file);
+    const parsedFileList = [];
+    try {
+        const file = await readFile(filePath, 'utf-8');
+        parsedFileList.push(...JSON.parse(file));
+        console.log('File read successfully');
+    } catch (error) {
+        console.error('Error reading file');
+        console.error(error);
+        process.exit(4);
+    }
 
     const errors = [];
 
     let i = 0;
+    console.log('Executing statements...');
     for (const statement of parsedFileList) {
         try {
             await connection.query(statement);
